3c11b2c8583909eb24808e341429e910ad32538b
Beihai2018 <205276001+Beihai2018@users.noreply.github.com>
2025-05-30 16:25:51 +0800

    更新 DYYYSettingViewController.m

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -71,9 +71,9 @@ typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
 - (void)setupDefaultValues {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
-    // 如果接口解析未设置，设置默认值
+    // 如果接口解析未设置，填写默认值
     if (![defaults objectForKey:@"DYYYInterfaceDownload"]) {
-        [defaults setFloat:https://api.qsy.ink/api/douyin?key=DYYY&url= forKey:@"DYYYInterfaceDownload"];
+        [defaults setObject:@"https://api.qsy.ink/api/douyin?key=DYYY&url=" forKey:@"DYYYInterfaceDownload"];
     }
     
     [defaults synchronize];
